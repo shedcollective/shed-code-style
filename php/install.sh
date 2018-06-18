ce2f4fb19e9063e6b03b6090bca288cae9f2bcf4
Shaun McWhinnie <shaun@shedcollective.org>
2018-06-18 17:22:46 +0100

    feat(PHP): Added updated PHPCS config

--- php/install.sh
+++ php/install.sh
@@ -1,7 +1,11 @@
 # PHP
 echo "[Installing PHP linter]"
+mkdir -p "$HOME/.phpcs"
 installBin "phpcs" "brew install php-code-sniffer"
-createLink "Shed" "/usr/local/etc/php-code-sniffer/Standards" "$ROOT_DIR/php/Shed"
+createLink "Shed" "$HOME/.phpcs" "$ROOT_DIR/php/Shed"
+
+echo "Adding path to standard"
+phpcs --config-set installed_paths "$HOME/.phpcs"
 
 echo "Setting as default standard"
 phpcs --config-set default_standard $NAME
