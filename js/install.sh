5f329446e2e0b3ada7063bfac5b4af88e63bb393
Shaun McWhinnie <shaun@shedcollective.org>
2018-07-10 22:10:08 +0100

    feat(js): Added language support for Vue files.

--- js/install.sh
+++ js/install.sh
@@ -1,5 +1,6 @@
 # JS
 echo "[Installing JS linter]"
-installBin "eslint"  "npm install -g eslint"
+installBin "eslint"  "npm install -g eslint eslint-plugin-vue"
+installBin "eslint-plugin-vue"  "npm install -g eslint-plugin-vue"
 createLink ".eslintrc" "$HOME" "$ROOT_DIR/js/.eslintrc"
 echo ""
